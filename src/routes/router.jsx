b5c90b3187c022f12a2c271f06faf9640cefee67
mehefujali <mehefujalim@gmail.com>
2025-02-20 00:21:32 +0530

    react router setup complite

---
+++ src/routes/router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+      {
+            path:'/',
+            element: <h1>hello</h1>
+      }
+])
+
+
+export default router
