6d47107f1cf47ab3f80ca7a9dc5e2539e48cd987
mehefujali <mehefujalim@gmail.com>
2025-02-22 16:34:20 +0530

    login page modifyed

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,24 +1,33 @@
 import { useContext } from "react";
-import MyButton from "../Shaird/MyButton";
+
 import { AuthContext } from "../Context/AuthProvider";
 import { Navigate } from "react-router-dom";
-
+import { Button } from "@material-tailwind/react";
+import { FcGoogle } from "react-icons/fc";
 const Login = () => {
-      const {signInWithGoogle , user} = useContext(AuthContext)
-      const handleGoogleLogin = () => {
-            signInWithGoogle()
-      }
+  const { signInWithGoogle, user } = useContext(AuthContext);
+  const handleGoogleLogin = () => {
+    signInWithGoogle();
+  };
 
-    if(user){
-      return <Navigate to="/"/>
-    }
-      return (
+  if (user) {
+    return <Navigate to="/" />;
+  }
+  return (
+    <div className=" flex w-full h-screen justify-center items-center">
+      <div className=" w-fit h-fit p-4 ">
             <div>
-                  <div onClick={handleGoogleLogin}>
-                  <MyButton >Google login</MyButton>
-                  </div>
+            <h1 className=" text-xl lg:text-3xl font-bold theme-text text-center">Welcome to Taskify!</h1>
+            <p>Simplify your workflow—log in with Google and get started.</p>
             </div>
-      );
+        <div className=" cursor-pointer w-fit mx-auto mt-5" onClick={handleGoogleLogin}>
+          <Button className=" flex items-center gap-2 rounded bg-primary-light cursor-pointer">
+            <FcGoogle className=" text-xl" /> Continue with google
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
